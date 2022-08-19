ce4911cec840857f930714629a3d0b414308eebe
Jay Grace <mjzakar@gmail.com>
2022-08-20 03:29:49 +0430

    added different storage options

---
+++ lib/ResultPage.dart
@@ -0,0 +1,182 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+
+class ResultPage extends StatefulWidget {
+  ResultPage({Key? key}) : super(key: key);
+
+  @override
+  State<ResultPage> createState() => _ResultPageState();
+}
+
+class _ResultPageState extends State<ResultPage> {
+  late List<dynamic> dbList;
+  Map<String, dynamic> fileList = {};
+  Future<void> getDbData() async {
+    final database = openDatabase(
+      join(await getDatabasesPath(), 'birthday_database.db'),
+    );
+    final db = await database;
+    final List birthdays = await db.query('birthday');
+    dbList = birthdays;
+  }
+
+  getSharedPrefsData() async {
+    final shared_prefs = await SharedPreferences.getInstance();
+    return shared_prefs.getString('date');
+  }
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/counter.json');
+  }
+
+  Future<void> readFromFile() async {
+    final file = await _localFile;
+    if (file.existsSync() && file.readAsStringSync().isNotEmpty) {
+      fileList = jsonDecode(file.readAsStringSync());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Results'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  color: Colors.blue.shade300,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'SharedPreferences:',
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                        height: 100,
+                        width: 150,
+                        child: Center(
+                          child: FutureBuilder(
+                            future: getSharedPrefsData(),
+                            builder: (context, key) {
+                              switch (key.connectionState) {
+                                case ConnectionState.waiting:
+                                  return CircularProgressIndicator();
+                                default:
+                                  return key.hasData && key.data != null
+                                      ? Text(DateFormat.yMMMMd().format(
+                                          DateTime.parse(key.data.toString())))
+                                      : Text('no data');
+                              }
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.all(20.0),
+                color: Colors.blue.shade300,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      'From File: ',
+                      textAlign: TextAlign.center,
+                      textScaleFactor: 1,
+                    ),
+                    SizedBox(
+                      height: 100,
+                      width: 150,
+                      child: Center(
+                        child: FutureBuilder(
+                          future: readFromFile(),
+                          builder: (context, key) {
+                            switch (key.connectionState) {
+                              case ConnectionState.waiting:
+                                return CircularProgressIndicator();
+                              default:
+                                return fileList.length != 0
+                                    ? Text(DateFormat.yMMMMd().format(
+                                        DateTime.parse(fileList['birthday'])))
+                                    : Text('no data');
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'From SQlite:',
+                  textAlign: TextAlign.center,
+                  textScaleFactor: 1,
+                ),
+              ),
+              Card(
+                child: SizedBox(
+                  height: 250,
+                  width: 200,
+                  child: FutureBuilder(
+                    future: getDbData(),
+                    builder: (context, key) {
+                      switch (key.connectionState) {
+                        case ConnectionState.waiting:
+                          return CircularProgressIndicator();
+                        default:
+                          return ListView.builder(
+                            itemCount: dbList.length,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                  padding: EdgeInsets.all(10),
+                                  child: Container(
+                                    padding: EdgeInsets.all(8),
+                                    decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.blue),
+                                    ),
+                                    child: Text(
+                                      DateFormat.yMMMMd().format(
+                                          DateTime.tryParse(
+                                                  dbList[index]['birthdate']) ??
+                                              DateTime.now()),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ));
+                            },
+                          );
+                      }
+                    },
+                  ),
+                ),
+              ),
+            ]),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,156 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sqflite/sqflite.dart';
+import 'dart:io';
+
+import 'package:storage_practice/ResultPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Storage Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Storage Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final birthDateController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  var birthDate;
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/counter.json');
+  }
+
+  Future<File> writeToFile() async {
+    final file = await _localFile;
+    if (!file.existsSync()) file.createSync();
+    return file.writeAsString(jsonEncode({'birthday': birthDate.toString()}),
+        mode: FileMode.writeOnly);
+  }
+
+  void saveToStorage() async {
+    //add to shared preferences
+    final shared_preferences = await SharedPreferences.getInstance();
+    shared_preferences.setString('date', birthDate.toString());
+    print('dont writing prefs');
+    //add to sqlite db
+    final database = openDatabase(
+      join(await getDatabasesPath(), 'birthday_database.db'),
+      onCreate: (db, version) {
+        return db.execute(
+            'CREATE TABLE IF NOT EXISTS birthday(id INTEGER PRIMARY KEY AUTOINCREMENT, birthdate TEXT)');
+      },
+      version: 1,
+    );
+    final db = await database;
+    await db.insert('birthday', {'birthdate': birthDate.toString()});
+    print('done writing db'); //add to file
+    await writeToFile();
+    print('done writing file');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Card(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Form(
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 15, horizontal: 30),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            border: UnderlineInputBorder(),
+                            labelText: 'birthday'),
+                        readOnly: true,
+                        onTap: () async {
+                          DateTime? date = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime(1900),
+                              lastDate: DateTime.now());
+                          if (date != null) {
+                            setState(() {
+                              birthDateController.text =
+                                  DateFormat.yMMMMd().format(date);
+                              birthDate = date;
+                            });
+                          }
+                        },
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          saveToStorage();
+                        }
+                      },
+                      style: ButtonStyle(
+                          shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20)))),
+                      child: const Text('Save Birthday'),
+                    ),
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => ResultPage()));
+                },
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)))),
+                child: const Text('See Stored Data'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
